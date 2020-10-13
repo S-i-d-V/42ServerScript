a324d82e5bbb34441fb5be9485194240b1aab2b5
Ugo THEVENY <ugtheven@e1r12p18.42.fr>
2020-10-13 16:40:39 +0200

    HTTPS Fixed | Installation pile LEMP

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM debian:buster
 
 #Copie des sources necessaires
 COPY srcs/server_conf /tmp/
-COPY srcs/private.key /tmp/
-COPY srcs/server.crt /tmp/
+COPY srcs/localhost.key /tmp/
+COPY srcs/localhost.crt /tmp/
 COPY srcs/script.sh ./
 
 #Lancement du script de SETUP.
--- srcs/script.sh
+++ srcs/script.sh
@@ -1,13 +1,14 @@
 #Installation et update des packets
 apt-get update
 apt-get install -y nginx
+apt-get install -y wget
+apt install -y mariadb-server mariadb-client
 
 #Set-up Nginx
-cp /tmp/server_conf /etc/nginx/sites-available/
+mv /tmp/server_conf /etc/nginx/sites-available/
 rm -rf /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
 cd /etc/nginx/sites-enabled/ && ln -s ../sites-available/server_conf .
 
-#Configuration SSL
-mkdir /etc/ssl
-cp /tmp/server.crt /etc/ssl
-cp /tmp/private.key /etc/ssl
+#Certificat SSL
+mv /tmp/localhost.crt /etc/ssl/certs/
+mv /tmp/localhost.key /etc/ssl/private/
