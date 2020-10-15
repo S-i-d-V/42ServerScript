91b3aecfeeaab41ab12d3f0623b6f68dc1528e4c
Ugo THEVENY <ugtheven@e1r7p4.42.fr>
2020-10-15 16:11:05 +0200

    Tt services installer, configuration requise

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,14 @@ FROM debian:buster
 
 #Copie des sources necessaires
 COPY srcs/server_conf /tmp/
-COPY srcs/localhost.key /tmp/
-COPY srcs/localhost.crt /tmp/
-COPY srcs/wordpress.tar.gz /tmp/
+COPY srcs/ssl/localhost.key /tmp/
+COPY srcs/ssl/localhost.crt /tmp/
 COPY srcs/script.sh ./
 
 #Lancement du script de SETUP.
 RUN bash script.sh
 
 #BASH
-CMD bash
+CMD service nginx start; \
+    service mysql restart; \
+    bash
--- srcs/script.sh
+++ srcs/script.sh
@@ -1,24 +1,30 @@
+#!/bin/bash
+
 #Installation et update des packets
 apt-get update
-apt-get install -y nginx
 apt-get install -y wget
-#apt-get install -y mariadb-server
-#apt-get install -y php7.3 php7.3-fpm php7.3-mysql php-mbstring php-json
+apt-get install -y nginx
+apt-get install -y mariadb-server
+apt-get install -y php-json php-mbstring
 
-#Set-up Nginx
+#Set-up Nginx + Certificat SSL
 mv /tmp/server_conf /etc/nginx/sites-available/
 rm -rf /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
 ln -s /etc/nginx/sites-available/server_conf /etc/nginx/sites-enabled/
-
-#Certificat SSL
 mv /tmp/localhost.crt /etc/ssl/certs/
 mv /tmp/localhost.key /etc/ssl/private/
 
 #Set-up Wordpress
 mkdir /var/www/localhost
-mv /tmp/wordpress.tar.gz  /var/www/localhost/
-tar xfC var/www/localhost/wordpress.tar.gz var/www/localhost/ && rm -rf /var/www/localhost/wordpress.tar.gz
+wget https://fr.wordpress.org/wordpress-5.5.1-fr_FR.tar.gz -P /var/www/localhost/
+tar xfC var/www/localhost/wordpress-5.5.1-fr_FR.tar.gz var/www/localhost/ && rm -rf /var/www/localhost/wordpress-5.5.1-fr_FR.tar.gz
 
 #Set-up PHPmyadmin
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.6/phpMyAdmin-4.9.6-english.tar.gz -P /var/www/localhost/
+tar xfC var/www/localhost/phpMyAdmin-4.9.6-english.tar.gz var/www/localhost/ && rm -rf /var/www/localhost/phpMyAdmin-4.9.6-english.tar.gz
 
 #Set-up MySQL
+service mysql start
+mysql -u root -e "CREATE DATABASE localhost_db;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON localhost_db.* TO 'localhost_admin'@'localhost' IDENTIFIED BY 'admin';"
+mysql -u root -e "FLUSH PRIVILEGES;"
